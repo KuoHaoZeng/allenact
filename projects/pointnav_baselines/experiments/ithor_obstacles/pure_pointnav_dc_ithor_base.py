79f827965823a06577b6104bb4538ce2df758ab4
khzeng <khzeng@khz.cs.washington.edu>
2021-03-11 05:37:49 -0800

    add dynamics corrpution 3

--- projects/pointnav_baselines/experiments/ithor_obstacles/pure_pointnav_dc_ithor_base.py
+++ projects/pointnav_baselines/experiments/ithor_obstacles/pure_pointnav_dc_ithor_base.py
@@ -32,7 +32,7 @@ class PointNaviThorBaseConfig(ObjectNavBaseConfig, ABC):
             #local_thor_build="/home/khzeng/exp/NPM/src/ai2thor/unity/builds/thor-local-Linux64",
         )
 
-        self.NUM_PROCESSES = 16
+        self.NUM_PROCESSES = 80
         self.TRAIN_GPU_IDS = list(range(torch.cuda.device_count()))
         #self.TRAIN_GPU_IDS = [1]
         self.VALID_GPU_IDS = [torch.cuda.device_count() - 1]
